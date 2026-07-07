64022d1723c08729a6b69b64fb9b05b2ca5fdead
Joshua Ensley <joshensley54@gmail.com>
2026-07-07 14:02:10 -0400

    Add files

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include "nri.h"
+#include <conio.h>
+
+//Counts down to zero from adress 8
+int countdown[32] = {0x08, 0xC4, 0xE3, 0x82, 0xFF, 0x00, 0x00, 0x00, 
+                     0x0F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+                     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+int main() {
+    NRI* nri = new NRI();
+
+    nri->set_memory(countdown);
+
+    int count = 0;
+    while(nri->get_mode() != HALT) {
+        getch();
+        std::cout<<nri->get_program_register()<<std::endl;
+        std::cout<<nri->get_accumulator()<<std::endl;
+        for(int i = 0; i < 16; i++) { nri->clock_cycle(); }
+    }
+
+    return 0;
+}
---
+++ src/nri.h
@@ -0,0 +1,86 @@
+#ifndef nri_h
+#define nri_h
+
+#include <iostream>
+
+const unsigned int MEM_SIZE = 32;
+
+enum Mode {
+    HALT,
+    FETCH,
+    EXECUTE
+};
+
+class NRI
+{
+    public:
+        NRI();
+
+        void clock_cycle();
+        
+        void set_memory(int* new_memory);
+
+        Mode get_mode();
+        unsigned int get_accumulator();
+        unsigned int get_program_register();
+        unsigned int get_timing_counter();
+    
+    private:
+        const unsigned int MAX_COUNT = 8;
+        const unsigned int RAM_BEGINS = 16;
+
+        Mode mode;
+
+        unsigned int accumulator = 0;
+        unsigned int register_b = 0;
+        unsigned int register_e = 0;
+
+        unsigned int instruction_register = 0;
+        unsigned int memory_address_register = 0;
+        unsigned int program_register = 0;
+
+        unsigned int timing_counter = 0;
+
+        unsigned int memory[MEM_SIZE];
+
+        void fetch();
+        void execute();
+
+        //OPCODES
+
+        void LDA();
+
+        void STA();
+
+        void ADD();
+
+        void SUB();
+
+        void JMP();
+
+        void JOM();
+
+        void JOZ();
+
+        void RAB();
+
+        void SHA();
+
+        void AND();
+
+        void DCA();
+
+        void CMA();
+
+        void RAE();
+
+        void HLT();
+
+        //Utilities
+        unsigned int safe_add(unsigned int a, unsigned int b);
+        unsigned int safe_sub(unsigned int a, unsigned int b);
+        
+
+};
+
+#endif
